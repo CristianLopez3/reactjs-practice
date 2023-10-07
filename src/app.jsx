df88eb68c48b46f7e30ee6d26f9546dad94d6acb
Cristian <cristian.c.lopez.m@gmail.com>
2023-10-07 17:46:40 -0500

    add components folder and create my first component

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'preact/hooks'
+import { useState } from 'preact/hooks';
+import React from "react";
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
-import './app.css'
+import './app.css';
+import Componente from './components/Componente';
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,8 @@ export function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>                 
+        </a>        
+        <Componente/>         
         <a href="https://preactjs.com" target="_blank">
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
@@ -26,9 +29,6 @@ export function App() {
           Edit <code>src/app.jsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Componente.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Componente extends Component {
+
+  render(){
+    return <h2>Hola, soy un Componente</h2>
+  }
+
+
+}
+
+export default Componente;
