6db7a648b0ed6d6135c269d146f7eba5d0d07c78
Cristian <cristian.c.lopez.m@gmail.com>
2023-10-11 00:12:52 -0500

    add ajax fetch api with class components

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,7 @@ import RenderizadoElementos from './components/RenderizadoElementos';
 import { EventosEM6, EventosEM7, MasSobreEventos } from './components/Eventos';
 import { Padre } from './components/ComunicacionEntreComponentes';
 import CicloVida from './components/CicloVida';
+import AjaxApis from './components/AjaxApis';
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -56,6 +57,8 @@ export function App() {
         <CicloVida />
         <hr />
 
+        <AjaxApis />
+        <hr />
 
       </section>
     </>
---
+++ src/components/AjaxApis.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+function Pokemon(props) {
+  return (
+    <figure>
+      <img src={props.url} alt={props.name} />
+      <figcaption>{props.name}</figcaption> {/* Corrección de la etiqueta */}
+    </figure>
+  );
+}
+
+export default class AjaxApis extends Component {
+  state = {
+    pokemons: [],
+  }
+
+  componentDidMount() {
+    let url = "https://pokeapi.co/api/v2/pokemon/";
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        json.results.forEach((el) => {
+          fetch(el.url)
+            .then((res) => res.json())
+            .then((json) => {
+              console.log(json);
+              let pokemon = {
+                id: json.id,
+                name: json.name,
+                url: json.sprites.front_default,
+              };
+              let pokemons = [...this.state.pokemons, pokemon];
+              this.setState({ pokemons }); // Corrección en la actualización del estado
+            })
+        })
+      })
+      .catch(err => { console.info(err) })
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Componentes de clase con Ajax</h2>
+        <p>Elementos pintados</p>
+        {
+          this.state.pokemons.length === 0 ?
+            (<h2>Cargando...</h2>) :
+            (this.state.pokemons.map((el) => {
+              return <Pokemon key={el.id} name={el.name} url={el.url} />; // Agregando 'return'
+            }))
+        }
+      </>
+    );
+  }
+}
