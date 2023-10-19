7d6f9c03d6b6c31e7c7eae252d877ecf9839f0ec
Cristian <cristian.c.lopez.m@gmail.com>
2023-10-18 23:01:42 -0500

    Boostrap via cdn

---
+++ react-frameworks/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Bootstrap from './components/Bootstrap'
+
+function App() {
+
+
+  return (
+    <>
+      <Bootstrap />
+      <h1>Css Frameworks with React.js</h1>
+    </>
+  )
+}
+
+export default App
