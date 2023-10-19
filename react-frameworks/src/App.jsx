0fcffedd8be6e67b3e9c496ebf3d6f75f1f28fb8
Cristian <cristian.c.lopez.m@gmail.com>
2023-10-19 11:56:55 -0500

    react front-end frameworks

--- react-frameworks/src/App.jsx
+++ react-frameworks/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-import Bootstrap from './components/Bootstrap'
+// import Bootstrap from './components/Bootstrap'
+import Bulma from './components/Bulma'
 
 function App() {
 
 
   return (
     <>
-      <Bootstrap />
+      {/* <Bootstrap /> */}
       <h1>Css Frameworks with React.js</h1>
+      <Bulma />
     </>
   )
 }
