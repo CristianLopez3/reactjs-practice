9cadd2eeb1d26f684b3a3c532ac0fb34fe7438f8
Cristian <cristian.c.lopez.m@gmail.com>
2023-10-19 22:29:38 -0500

    React bootstrap and Material ui

--- react-frameworks/src/App.jsx
+++ react-frameworks/src/App.jsx
@@ -1,7 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import MaterialUi from './components/MaterialUi'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import Bootstrap from './components/Bootstrap'
-import Bulma from './components/Bulma'
+// import Bulma from './components/Bulma'
+// import ReactBoostrap from './components/ReactBoostrap'
+
+
 
 function App() {
 
@@ -10,7 +16,9 @@ function App() {
     <>
       {/* <Bootstrap /> */}
       <h1>Css Frameworks with React.js</h1>
-      <Bulma />
+      {/* <Bulma /> */}
+      {/* <ReactBoostrap /> */}
+      <MaterialUi />
     </>
   )
 }
---
+++ react-frameworks/src/components/MaterialUi.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MaterialCard from './MaterialCard';
+import MaterialDrawer from './MaterialDrawer';
+
+export default function MaterialUi() {
+  return (
+    <>
+      <h2>material ui</h2>
+      <MaterialCard />
+      <br />
+      <br />
+      <MaterialDrawer />
+    </>
+  )
+}
