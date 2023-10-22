252028da35546ebd922fcfd3e8699cddec970b6b
Cristian <cristian.c.lopez.m@gmail.com>
2023-10-21 20:20:52 -0500

    json server in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import CrudApp from './components/CrudApp'
+import CrudApi from './components/CrudApi'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Crud with React</h1>
+      <CrudApi />
+      <hr />
       <CrudApp />
+      <hr />
     </>
   )
 }
---
+++ src/components/CrudApi.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+
+
+function CrudApi() {
+
+  const [db, setDb] = useState([]);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  /* Los elementos hijos se comunican a traves de funciones */
+  const createData = (data) => {
+    data.id = Date.now();
+    setDb([
+      ...db,
+      data
+    ])
+  }
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id) ? data : el);
+    setDb(newData);
+  }
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(`Are you sure of delete the register with id ${id}`);
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    } else {
+      return;
+    }
+  }
+
+  return (
+    <>
+      <h3> Crud Api </h3>
+
+      <article className="grid-1-2">
+
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          dataDb={db}
+          deleteData={deleteData}
+          setDataToEdit={setDataToEdit}
+          dataToEdit={dataToEdit}
+        />
+      </article>
+
+    </>
+  );
+}
+
+export default CrudApi;
