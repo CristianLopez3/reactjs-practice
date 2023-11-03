0a0ba1e5195f88f09b1b23d0fbe14f9d15ef4808
Cristian <cristian.c.lopez.m@gmail.com>
2023-11-02 20:23:37 -0500

    BrowserRouter, Routes, Route, Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
+function Acerca() {
+  return (
+    <>
+      <h2>acerca</h2>
+      <a href="/">home</a>
+    </>
+
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
         <h1>React Route</h1>
-      </div>      
+
+        <BrowserRouter>
+          <nav>
+            <Link to="/">Home / </Link> 
+            <Link to="/acerca">Acerca</Link>
+          </nav>
+          <Routes>
+            <Route path='/' element={<h2>home</h2>} />
+            <Route path='/acerca' element={<Acerca />} />
+          </Routes>
+        </BrowserRouter>
+
+      </div>
     </>
   )
 }
