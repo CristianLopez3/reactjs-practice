954f941abc44ca5cf62717f4cd4b0111fd61fd15
Cristian <cristian.c.lopez.m@gmail.com>
2023-12-21 18:11:39 -0500

    using payload in reducer function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,36 +1,54 @@
 import { useState, useReducer } from "react";
 
-const initialState = { counter: 0 };
-function reducer(state, action){
+const TYPES = {
+  INCREMENT: "INCREMENT",
+  INCREMENT_5: "INCREMENT_5",
+  DECREMENT: "DECREMENT",
+  DECREMENT_5: "DECREMENT_5",
+  RESET: "RESET",
+};
 
+const initialState = { counter: 0 };
+
+function reducer(state, action) {
   switch (action.type) {
-    case "INCREMENT":
-      return {counter: state.counter + 1};
-    case "DECREMENT":
-      return {counter: state.counter - 1};
+    case TYPES.INCREMENT:
+      return { counter: state.counter + 1 };
+    case TYPES.INCREMENT_5:
+      return { counter: state.counter + action.payload };
+    case TYPES.DECREMENT:
+      return { counter: state.counter - 1 };
+    case TYPES.DECREMENT_5:
+      return { counter: state.counter - action.payload }
+    case TYPES.RESET:
+      return { counter: initialState.counter }
     default:
       return state;
   }
-  
 }
-
 
 const Counter = () => {
   // const [counter, setCounter] = useState(0);
   const [state, dispatch] = useReducer(reducer, initialState);
 
   // const plus = () => setCounter(counter + 1);
-  const plus = () => dispatch({type: "INCREMENT"});
+  const plus = () => dispatch({ type: TYPES.INCREMENT });
   // const subtract = () => setCounter(counter - 1);
-  const subtract = () => dispatch({type: "DECREMENT"});
-  
+  const subtract = () => dispatch({ type: TYPES.DECREMENT });
+
+  const plus5 = () => dispatch({ type: TYPES.INCREMENT_5, payload: 5 });
+  const subtract5 = () => dispatch({ type: TYPES.DECREMENT_5, payload: 5 });
+  const reset = () => dispatch({ type: TYPES.RESET });
 
   return (
     <div>
       <h2>Counter</h2>
       <nav>
-        <button onClick={plus} >+</button>
-        <button onClick={subtract} >-</button>
+        <button onClick={plus}>+</button>
+        <button onClick={subtract}>-</button>
+        <button onClick={plus5} > +5 </button>
+        <button onClick={subtract5}> -5 </button>
+        <button onClick={reset}> 0 </button>
       </nav>
       <p>{state.counter}</p>
     </div>
